a312e12dbf8410eb02c69371743d93b1367ae5c7
DevourHavoc <160288271+DevourHavoc@users.noreply.github.com>
2024-04-20 22:14:00 +0100

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,3 @@
-.
+loadstring(game:HttpGet("", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DevourHavoc/HubX/main/Source.lua", true))()
+https://raw.githubusercontent.com/DevourHavoc/HubX/main/Source.lua
