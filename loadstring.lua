1ba9beb150220094a8013d5fec431d23c4e9fafc
DevourHavoc <160288271+DevourHavoc@users.noreply.github.com>
2024-04-21 00:56:11 +0100

    loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,3 +1,9 @@
+None Obfuscated
 loadstring(game:HttpGet("", true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/DevourHavoc/HubX/main/Source.lua", true))()
 https://raw.githubusercontent.com/DevourHavoc/HubX/main/Source.lua
+
+Obfuscated 
+loadstring(game:HttpGet("", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DevourHavoc/HubX-Official/main/HubX.lua", true))()
+https://raw.githubusercontent.com/DevourHavoc/HubX-Official/main/HubX.lua
